a8da032a3e38c78362a58650f2b49b626cfefd36
Ms.Bithi <bithimony01904@gmail.com>
2024-10-31 16:24:09 +0600

    calculator mini project done

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /Calculator/
+        public IActionResult Index()
+        {
+            return View(new calculatorModel());
+
+        }
+
+        // POST: /Calculator/Calculate
+        [HttpPost]
+        public IActionResult Calculate(calculatorModel model)
+        {
+            switch (model.Operation)
+            {
+                case "Add":
+                    model.Result = model.Number1 + model.Number2;
+                    break;
+                case "Subtract":
+                    model.Result = model.Number1 - model.Number2;
+                    break;
+                case "Multiply":
+                    model.Result = model.Number1 * model.Number2;
+                    break;
+                case "Divide":
+                    if (model.Number2 != 0)
+                    {
+                        model.Result = model.Number1 / model.Number2;
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Cannot divide by zero.");
+                    }
+                    break;
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ Calculator/Models/calculatorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Models
+{
+    public class calculatorModel
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double Result { get; set; }
+        public string Operation { get; set; }
+    }
+}
